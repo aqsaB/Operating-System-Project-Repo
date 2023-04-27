80347c62bc72b3fcf3214f76eadb1467ff35d238
Aqsa Bhimdiwala <abhimdi@silo.luddy.indiana.edu>
2023-04-27 11:41:26 -0400

    files progress

--- lib/fs_write.c
+++ lib/fs_write.c
@@ -14,6 +14,36 @@ extern filetable_t oft[NUM_FD];
  *        *   - Update inode state to reflect changes
  *         *   - Return the number of bytes written
  *          */
+int get_free_block(){
+	for(int k = 0; k<fsd->device.nblocks; k++){
+		if(fs_getmaskbit(k) == 0){
+			return k;
+		}
+	}
+	return -1;
+}
 int fs_write(int fd, char* buff, int len) {
-	  return 0;
+	int counter = 0;
+	int write_sz = 1;
+	int head = oft[fd].fileptr;
+	int block_sz = fsd->device.blocksz;
+
+	int total = len;
+	
+	while(total>0){
+		/*if (head%block_sz == 0 head > oft[fd].in.size){
+			int page_index = head/block_sz;
+			oft[fd].in.blocks[page_index];	
+		}*/
+
+		bs_write(oft[fd].in.blocks[head/block_sz], head%block_sz, &buff[counter], write_sz);
+		head += write_sz;
+		counter += 1;
+		total-=1;
+
+	}
+	oft[fd].fileptr = head;
+	oft[fd].in.size = head;
+
+	return counter;
 }
